353236ef7e5bea7ded5f454ee556c471e825c7c7
black-ne-ko <reu7494@naver.com>
2025-07-22 12:15:28 +0900

    .

--- todolist3/src/App.js
+++ todolist3/src/App.js
@@ -2,25 +2,35 @@ import { useState } from "react";
 
 export default function App() {
   const [text, setText] = useState("");
-  const [list, setList] = useState("");
+  const [lists, setLists] = useState({
+    id: 0,
+    list: "",
+  });
+
+  function ListsInput(id) {
+    setLists((prev) => ({
+      ...prev,
+      id: ++id,
+      [id]: text,
+    }));
+    setText("");
+    console.log(id);
+  }
 
   return (
-    <>
+    <div>
       <input
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="입력"
       />
-      <button
-        onClick={() => {
-          setList(text);
-          setText("");
-        }}
-      >
-        입력
-      </button>
-      <p>{list}</p>
-    </>
+      <button onClick={() => ListsInput(lists.id)}>입력</button>
+      <ul>
+        <li key={lists.id}>
+          <p>{lists[lists.id]}</p>
+        </li>
+      </ul>
+    </div>
   );
 }
